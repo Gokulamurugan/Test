dbe74f87e6a7f409df0359eeb2f48c89dd611daf
Gokulamurugan <gokulamurugan1@gmail.com>
2022-03-15 00:27:20 +0000

    first upload

--- PT2/tests/prog.spec.ts
+++ PT2/tests/prog.spec.ts
@@ -8,6 +8,6 @@ test('basic test', async ({ page }) => {
     await page.goto('https://playwright.dev/');
     const title = page.locator('.navbar__inner .navbar__title');
     await expect(title).toHaveText('Playwright');
-    //Test sample FGH
+    //Test sample FGH kk
 
 });
